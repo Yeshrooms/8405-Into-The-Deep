aae8ac7e5f8cd96f8daa1ebd0ad95ce0f077d00a
welu28 <27luw@millburn.org>
2024-12-12 23:57:14 -0500

    kms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/CameraTest.java
@@ -17,24 +17,19 @@ public class CameraFeedDisplay extends OpMode {
         telemetry.addLine("Initializing...");
         telemetry.update();
 
-        // Get the camera monitor view ID (needed to display the camera feed on the Driver Station)
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
                 "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
 
-        // Create the webcam instance using EasyOpenCV
         webcam = OpenCvCameraFactory.getInstance().createWebcam(
                 hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
-        // Set the webcam's pipeline (to just show the frame as it is, without processing)
-        webcam.setPipeline(new OpenCvCamera.PipelinOvee() {
+        webcam.setPipeline(new OpenCvCamera.Pipeline() {
             @Override
             public Mat processFrame(Mat inputFrame) {
-                // No processing, just return the frame
                 return inputFrame;
             }
         });
 
-        // Open the camera and start streaming
         webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
@@ -53,13 +48,10 @@ public class CameraFeedDisplay extends OpMode {
 
     @Override
     public void loop() {
-        // Camera feed will automatically be shown on the Driver Station screen.
-        // You can use this method to add more logic if needed during the TeleOp period.
     }
 
     @Override
     public void stop() {
-        // Stop the camera when the OpMode is stopped
         if (webcam != null) {
             webcam.stopStreaming();
             webcam.closeCameraDevice();
